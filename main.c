e0b716aa859b8d351a7ae6139e3da0c989cfb45a
Kyu Young Park <kyuyoungpark@Kyuui-MacBookAir.local>
2021-09-15 12:00:49 -0500

    working files

--- main.c
+++ main.c
@@ -6,23 +6,9 @@ const int MAX_WORDS = 500000;
 const int MAX_LETTERS_PER_WORD = 255;
 const int LINE = 10000;
 
-int separateString(char* str)
+void modifyTimeStamp(char *line, char *str, int index)
 {
-   char* token = strtok(str, " ");
-   while(token!=NULL)
-   {
-      printf("%s\n", token);
-      token = strtok(NULL, " ");
-   }
-
-   return 0;
-}
-
-
-void modifyTimeStamp(char* line, char* str, int index)
-{
-  
-   if(index==-1)
+   if (index == -1)
    {
       strcat(str, line);
    }
@@ -31,76 +17,74 @@ void modifyTimeStamp(char* line, char* str, int index)
    {
       char before[255];
       char after[255];
-      
+      char modified[20];
       memset(before, '\0', sizeof(before));
       memset(after, '\0', sizeof(after));
-      
-      //printf("%d\n", '\0'==after[5]);
-      strncpy(before, line, index-2);
-      strncpy(after, line + (index-2), 255 - (index - 2));
-      
-      char modified[20];
       memset(modified, '\0', sizeof(modified));
 
-      if(strlen(after) < 10)
-      {  
+      strncpy(before, line, index - 2);
+      strncpy(after, line + (index - 2), 255 - (index - 2));
+
+      if (strlen(after) < 7)
+      {
          strcpy(modified, "00:");
          strcat(modified, after);
+         strcat(before, modified);
+         strcat(str, before);
       }
-      strcat(before, modified);
-      strcat(str, before);
+      else
+      {
+         strcat(str, line);
+      }
    }
 }
 
 int firstCollonIndex(char *str)
 {
    const char COLLON = ':';
-   char* temp;
+   char *temp;
    int index;
 
    temp = strchr(str, COLLON);
-   if(temp==NULL)
+   if (temp == NULL)
       return -1;
+
    index = (int)(temp - str);
    return index;
 }
 
-
-
-
 int main()
 {
-   
    char str[MAX_CHAR_LENGTH];
+   memset(str, '\0', sizeof(str));
+   char line[MAX_LETTERS_PER_WORD];
+   char arr[LINE][MAX_LETTERS_PER_WORD];
+   char fileName[100];
+
+   printf("File Name?: ");
+   scanf("%s", fileName);
 
    FILE *file;
-   file = fopen("test.txt", "r");
+   file = fopen(fileName, "r");
 
-   if(file==NULL)
+   if (file == NULL)
    {
       printf("No file found");
       return -1;
    }
-   
-   char line[MAX_LETTERS_PER_WORD];
-   char arr[LINE][MAX_LETTERS_PER_WORD];
-   
-   while(fgets(line, sizeof(line), file)!=NULL)
+
+   while (fgets(line, sizeof(line), file) != NULL)
    {
-      
+
       int i = firstCollonIndex(line);
       modifyTimeStamp(line, str, i);
    }
 
    FILE *out;
-   out = fopen("output.txt", "w+");
+   out = fopen(strcat(fileName, "_modified"), "w+");
    fputs(str, out);
+
    fclose(out);
    fclose(file);
-
-   
    return 0;
 }
-
-
-
